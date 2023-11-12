ee79ef8fee0febd63c4181432e70b2c7d9813354
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2023-11-12 12:18:49 -0300

    feat: adding data scrapping code

--- scraper/university_scraper.py
+++ scraper/university_scraper.py
@@ -1,5 +1,6 @@
 import requests
 from _helpers.logger import setup_logger
+from bs4 import BeautifulSoup
 
 logger = setup_logger()
 
@@ -7,7 +8,6 @@ logger = setup_logger()
 class UniversityScraper:
     def __init__(self, university_webpage_url: str):
         self._university_webpage_url = university_webpage_url
-        pass
 
     def _fetch_webpage(self):
         """
@@ -21,7 +21,7 @@ class UniversityScraper:
             logger.info(f'SUCCESS ACCESSING {self._university_webpage_url}')
             return response.text
         except requests.RequestException as e:
-            logger.error(f'ERROR FETCHING {self._university_webpage_url}')
+            logger.error(f'ERROR OF BAD CONTENT ON {self._university_webpage_url}')
             return None
 
     def scrap_university_page(self):
@@ -30,4 +30,40 @@ class UniversityScraper:
 
         :return: webpage data in string format
         """
-        pass
+        page_content = self._fetch_webpage()
+
+        if page_content:
+            soup = BeautifulSoup(page_content, 'html.parser')
+            university_name = soup.find('h1').text.strip()
+            logger.info(f'UNIVERSITY NAME {university_name}')
+
+            months = soup.find_all('h3')
+            logger.info(f'MONTHS {months}')
+
+            data = []
+
+            for month in months:
+                month_name = month.text.strip()
+                data.append(f'\n{month_name}\n')
+                logger.info(f'MONTH NAME {month_name}')
+
+                table = month.find_next('table', {'class': 'category'})
+                if table:
+                    rows = table.find('tbody', {'class': 'listras'}).find_all('tr')
+
+                    for row in rows:
+                        day = row.find('td', {'class': 'cell'})
+                        day_description = day.find_next('td', {'class': 'cell'})
+
+                        if day and day_description:
+                            day_text = day.text.strip()
+                            day_description_text = day_description.text.strip()
+                            data.append(f'{day_text}: {day_description_text}\n')
+                            logger.info(f'{day_text}: {day_description_text}')
+                        else:
+                            logger.warning(f'INVALID ROW STRUCTURE, SKIPPING')
+
+            with open('data.txt', 'w', encoding='utf-8') as file:
+                file.write(''.join(data))
+        else:
+            logger.error(f'ERROR FETCHING {self._university_webpage_url}')
